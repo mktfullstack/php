26664f04a300747cfc84241b58d5e9ad50729227
smartconsulting <63222017+mktfullstack@users.noreply.github.com>
2022-07-10 12:07:08 -0500

    now

---
+++ skins/default/theme-specific/theme-login/theme-login.js
@@ -0,0 +1,24 @@
+/* global jQuery:false */
+/* global ANESTA_STORAGE:false */
+
+
+( function() {
+
+	"use strict";
+
+
+	jQuery('.loginmmessage .copy').each(function(){
+		jQuery('<span class="icon"></span>').appendTo(jQuery(this));
+	});
+
+	jQuery('.loginmmessage .copy').on('click', function() {
+		var item = jQuery(this);
+		var text = item.text();
+		navigator.clipboard.writeText(text);
+
+		item.addClass('copied');
+		setTimeout(function() {
+			item.removeClass('copied');
+		}, 3000);
+	});
+})();
